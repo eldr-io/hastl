07680761bec7f88c0c1f8384f061f65c714fa4b5
sigrdrifa <corynz@pm.me>
2024-06-03 22:26:55 +0100

    html endpoints

--- lib/Api/Templates/User.hs
+++ lib/Api/Templates/User.hs
@@ -1,1 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Api.Templates.User where
+
+import Models (User (userName))
+import Lucid (Html, div_, class_, p_, ToHtml (toHtml))
+
+renderUsers :: [User] -> Html () 
+renderUsers [] = div_ [class_ "users"] (p_ "No data")
+renderUsers users = div_ [class_ "users"] (do mapM_ renderUser users) 
+  where renderUser user = p_ (toHtml (userName user))
+  
